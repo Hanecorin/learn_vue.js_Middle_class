1791370467648484a5a39919aa70a13fe385e301
Hanecorin <sdh1541@naver.com>
2023-08-18 15:54:45 +0900

    vue-todo-refactoring/230818

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+      <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
         <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}"
           v-on:click="toggleComplete(todoItem, index)"></i>
         <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
@@ -15,36 +15,15 @@
 
 <script>
 export default {
-  data: function(){
-    return{
-      todoItems:[]
-    }
-  },
+  props: ['propsdata'],
   methods: {
     removeTodo: function(todoItem,index){
-      localStorage.removeItem(todoItem);
-      this.todoItems.splice(index,1);
+      this.$emit('removeItem', todoItem,index);
     },
-    toggleComplete: function(todoItem){
-      todoItem.completed = !todoItem.completed;
-      // 로컬 스토리지의 데이터를 갱신
-      localStorage.removeItem(todoItem.item);
-      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
-    }
-  },
-
-  created: function() {
-    if (localStorage.length > 0){
-      for (var i=0; i<localStorage.length; i++){
-        if (localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-          // this.todoItems.push(localStorage.key(i));
-        }
-        
-      }
+    toggleComplete: function(todoItem,index){
+      this.$emit('toggleItem', todoItem,index);
     }
   }
-
 }
 </script>
 
